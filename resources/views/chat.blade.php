9252d12ea4a89a9a8361831ba9dd8cbc7cdb772e
Ahmed Adel <ahmedcrspo55@gmail.com>
2021-09-26 15:53:01 +0200

    Styling Chat Scroll

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -7,18 +7,29 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style>
+        .list-group{
+            overflow-y: scroll;
+            height: 500px;
+            border-left: black solid 1px;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <div class="row" id="app">
-            <ul class="list-group offset-4 col-4 mt-4">
-                <li class="list-group-item active">Chat Room</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Morbi leo risus</li>
-                <li class="list-group-item">Porta ac consectetur ac</li>
-                <li class="list-group-item">Vestibulum at eros</li>
-                <input type="text" placeholder="Type some text here..." class="form-control">
-              </ul>
+            <div class="offset-2 col-8 mt-4">
+                <ul class="list-group" v-chat-scroll>
+                    <li class="list-group-item active">Chat Room</li>
+                    <message-component
+                    v-for="(value,index) in chat.message" :key="index"
+                    color="success"
+                    >
+                    @{{ value }}
+                    </message-component>
+                </ul>
+                <input type="text" v-model="message" @keyup.enter="send" placeholder="Type some text here..." class="form-control">
+            </div>
         </div>
     </div>
 
